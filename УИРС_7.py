7a6fc78e211184cac1ab18c8ba1c44971514173c
Flarinli <maxim.v.filatov@outlook.com>
2021-07-19 14:30:31 +0300

    bugfix: rewrote half_division_method

--- УИРС_7.py
+++ УИРС_7.py
@@ -1,10 +1,18 @@
+import math
+def half_division_method(fn, a, b, e, small_num):
+    while math.fabs(a - b) > e:
+        y = (a + b - small_num) / 2
+        z = (a + b + small_num) / 2
+        if fn(y) < fn(z):
+            b = z
+        else:
+            a = y 
+    return (a + b) / 2, fn((a + b) / 2)
 
-def f(x):
-    return(127 / 4 * x**2 - 61 / 4 * x + 2)
-
-def half_divide_method(a, b, f):
-    x = (a + b) / 2
-    while math.fabs(f(x)) >= e:
-        x = (a + b) / 2
-        a, b = (a, x) if f(a) * f(x) < 0 else (x, b)
-    return (a + b) / 2
+if __name__ == '__main__':
+    def f(x):
+        return(127 / 4 * x**2 - 61 / 4 * x + 2)
+    e = 0.00001
+    a = -5
+    b = 5
+    print(half_division_method(f, a, b, e, e / 2))
